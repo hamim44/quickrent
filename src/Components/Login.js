9c1db0d941e748aa47ae6f485d7ed99ae1bd18c5
hamim44 <abrarhamim4044h@gmail.com>
2022-12-05 00:24:07 +0600

    login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,8 @@
 import React, {useState, userEffect} from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import './login.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const Login = ()=>{
     let[token, setToken]= useState("");
@@ -30,15 +32,40 @@ const Login = ()=>{
 
     }
     return(
-        <div>
-            <form>
-                <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
-                <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)}></input>
+        <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Log In</h3>
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                onChange={e => setEmail(e.target.value)} 
+                className="form-control mt-1"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password" onChange={e => setPassword(e.target.value)}
+                className="form-control mt-1"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button onClick={loginSubmit} className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+            <p className="forgot-password text-right mt-2">
+              Forgot <a href="#">password?</a>
+            </p>
+          </div>
+        </form>
+      </div>
 
-            </form>
-                <button onClick={loginSubmit}>Login</button>
-        </div>
+    );
+  };
 
-    )
-}
 export default Login;  
